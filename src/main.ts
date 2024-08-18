e4b6b38fe09baec6b7e8883a72f506fc3a68b236
bamidele2161 <akinyemibamidele2@gmail.com>
2024-08-18 16:08:54 +0100

    port

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   const port = process.env.PORT || 3000;
-  await app.listen(port, () => {
+  await app.listen(port, '0.0.0.0', () => {
     console.log(`listening on port ${port}`);
   });
 }
